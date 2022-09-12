7b6b3b9686172764a7ceda9b3eb24217c46e265e
Krisztina Vajda Rygård <krry@hesehus.dk>
2022-09-12 10:07:23 +0200

    Add support color palettes with HSL colors and optimize theming in 3 tiers

--- my-app/src/pages/App/index.tsx
+++ my-app/src/pages/App/index.tsx
@@ -20,6 +20,19 @@ function Index() {
 					<h2>Tier 3</h2>
 					<p className={styles.tier3}>--buttonPrimaryBackground</p>
 				</div>
+				<div className={styles.themingWrapper}>
+					<h2>Stateful colors</h2>
+					<p style={{ background: 'var(--colorSurfaceStatePrimary)' }}>--colorSurfaceStatePrimary</p>
+					<p style={{ background: 'var(--colorSurfaceStatePrimaryHover)' }}>
+						--colorSurfaceStatePrimaryHover
+					</p>
+					<p style={{ background: 'var(--colorSurfaceStatePrimaryFocus)' }}>
+						--colorSurfaceStatePrimaryFocus
+					</p>
+					<p style={{ background: 'var(--colorSurfaceStatePrimaryPressed)' }}>
+						--colorSurfaceStatePrimaryPressed
+					</p>
+				</div>
 				<img className={styles.logo} src={logo} alt="logo" />
 				<p>
 					Edit <code>src/App.tsx</code> and save to reload.
